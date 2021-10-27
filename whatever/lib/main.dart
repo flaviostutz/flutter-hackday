a567abdd25e8073efa28972cccfacc92332902ea
Flores Junior, S.E. (Sergio) <sergio.flores@nn.nl>
2021-10-27 13:59:12 +0200

    remove useless class

--- whatever/lib/main.dart
+++ whatever/lib/main.dart
@@ -25,11 +25,3 @@ class HomePage extends StatelessWidget {
     return const FormWidgetsDemo();
   }
 }
-
-class Demo {
-  final String name;
-  final String route;
-  final WidgetBuilder builder;
-
-  const Demo({required this.name, required this.route, required this.builder});
-}
